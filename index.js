b12a5b04bf57560d8959a9935204a840a146d122
Макушенко Матвей <matvey.myown@gmail.com>
2025-12-16 18:41:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const button = document.getElementById('mybutton')
+const showbutton = document.getElementById('showbutton')
 const input = document.getElementById('myinput')
 
 button.addEventListener('click', function () {
+	debugger
 	const currentValue = input.value
-	localStorage.setItem('Text', currentValue)
+	if (localStorage.getItem('Text')) {
+		const array = JSON.parse(localStorage.getItem('Text'))
+		array.push(currentValue)
+		localStorage.setItem('Text', JSON.stringify(array))
+	} else {
+		localStorage.setItem('Text', currentValue)
+	}
+})
+
+showbutton.addEventListener('click', function () {
+	let storage = JSON.parse(localStorage.getItem('Text'))
 })
